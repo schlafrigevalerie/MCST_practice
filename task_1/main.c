1643ab0b154ee2abcc50865c46204366ab60a8d5
Valerie <13012005lera@gmail.com>
2025-04-16 20:50:19 +0300

    creating_array

--- task_1/main.c
+++ task_1/main.c
@@ -1,5 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void marge()
+{
+
+}
+
+void merge_sort(){
+
+}
+
 int main()
 {
-    return 0;
+    srand(time(NULL));
+    int size = rand() % 10 + 1;
+    int* num = malloc(size * sizeof(int));
+    int i;
+    for (i = 0; i < size; i++){
+        num[i] = rand()%100;
+        printf("%d\n", num[i]);
+    };
+    free(num);
 }
 
