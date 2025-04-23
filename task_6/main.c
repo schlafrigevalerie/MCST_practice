09ab3c51e47250336c5baec43a58424f22e3953b
Valerie <13012005lera@gmail.com>
2025-04-23 20:52:27 +0300

    task_4_check_entered_data_and_arguments

--- task_6/main.c
+++ task_6/main.c
@@ -10,28 +10,43 @@
 void file_type(mode_t mode) {
     switch (mode & S_IFMT) {
         case S_IFREG:
-            printf("Тип: Обычный файл\n");
+            printf("Тип: обычный файл\n");
             break;
         case S_IFDIR:
-            printf("Тип: Каталог\n");
+            printf("Тип: каталог\n");
             break;
         case S_IFCHR:
-            printf("Тип: Символьное устройство\n");
+            printf("Тип: символьное устройство\n");
             break;
         case S_IFBLK:
-            printf("Тип: Блочное устройство\n");
+            printf("Тип: блочное устройство\n");
             break;
         case S_IFIFO:
-            printf("Тип: Канал FIFO\n");
+            printf("Тип: канал FIFO\n");
             break;
         case S_IFLNK:
-            printf("Тип: Символическая ссылка\n");
+            printf("Тип: символическая ссылка\n");
             break;
         case S_IFSOCK:
-            printf("Тип: Сокет\n");
+            printf("Тип: сокет\n");
             break;
         default:
-            printf("Тип: Неизвестно\n");
+            printf("Тип: неизвестно\n");
             break;
+    }
+}
+
+int main(int argc, char *argv[]){
+    //проверка данных
+    if (argc != 2) {
+        fprintf(stderr, "Некоректные данные\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat fileStat;
+    //проверяем успешность завершения вызова
+    if (stat(argv[1], &fileStat) == -1) {
+        perror("stat");
+        exit(EXIT_FAILURE);
     }
 }
