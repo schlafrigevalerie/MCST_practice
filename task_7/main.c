398714e9bf0fa7c7bbc9d951c3ce623aae2d23b0
Valerie <13012005lera@gmail.com>
2025-04-24 00:59:24 +0300

    task_7_add_check_file

--- task_7/main.c
+++ task_7/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 
 void copy_and_print(const char* input_file, const char* output_file, int print_after) {
     int in_fd = open(input_file, O_RDONLY);
+    if (in_fd < 0) {
+        perror("Ошибка при открытии входного файла");
+        exit(EXIT_FAILURE);
+    }
 
     int out_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (out_fd < 0) {
+        perror("Ошибка при создании выходного файла");
+        close(in_fd);
+        exit(EXIT_FAILURE);
+    }
 
     char buffer[1024];
     ssize_t bytes_read;
@@ -32,4 +42,3 @@ void copy_and_print(const char* input_file, const char* output_file, int print_a
         }
     }
 }
-
