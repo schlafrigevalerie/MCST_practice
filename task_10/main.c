b0e7717f4ae193304113e7734c7a9f0a9acd82e2
Valerie <13012005lera@gmail.com>
2025-04-25 20:38:23 +0300

    task_10_to_rpn

--- task_10/main.c
+++ task_10/main.c
@@ -1,9 +1,18 @@
+#include <ctype.h>
 #include <string.h>
 
 typedef struct {
     char name;
     int value;
 } Variable;
+
+int get_var_value(char var, Variable *vars, int count) {
+    for (int i = 0; i < count; i++) {
+        if (vars[i].name == var)
+            return vars[i].value;
+    }
+    return 0;
+}
 
 void normalize(char *expr) {
     char tmp[512];
@@ -24,6 +33,43 @@ void normalize(char *expr) {
     }
     tmp[j] = '\0';
     strcpy(expr, tmp);
+}
+
+int precedence(char op) {
+    switch (op) {
+        case '!': return 3; // NOT
+        case '&': return 2; // AND
+        case '|': return 1; // OR
+        default: return 0;
+    }
+}
+
+void to_rpn(const char *infix, char *postfix) {
+    char ops[512];
+    int top = -1, j = 0;
+    for (int i = 0; infix[i]; i++) {
+        char ch = infix[i];
+        if (isspace(ch)) continue;
+        if (isalpha(ch)) {
+            postfix[j++] = ch;
+        } else if (ch == '(') {
+            ops[++top] = ch;
+        } else if (ch == ')') {
+            while (top >= 0 && ops[top] != '(') {
+                postfix[j++] = ops[top--];
+            }
+            top--;
+        } else {
+            while (top >= 0 && precedence(ops[top]) >= precedence(ch)) {
+                postfix[j++] = ops[top--];
+            }
+            ops[++top] = ch;
+        }
+    }
+    while (top >= 0) {
+        postfix[j++] = ops[top--];
+    }
+    postfix[j] = '\0';
 }
 
 int main() {
