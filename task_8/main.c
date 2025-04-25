db1c3c634836101816ac2f225def9c643e4d7e1b
Valerie <13012005lera@gmail.com>
2025-04-25 19:31:48 +0300

    tasl_8_add_handler_sigint

--- task_8/main.c
+++ task_8/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 
@@ -14,45 +13,4 @@ void sigint_handler(int sig) {
         signal(SIGINT, SIG_DFL);  // Восстановить поведение по умолчанию
         sigint_count++;
     }
-}
-
-int main() {
-    int **A = malloc(SIZE * sizeof(int*));
-    int **B = malloc(SIZE * sizeof(int*));
-    int **C = malloc(SIZE * sizeof(int*));
-    for (int i = 0; i < SIZE; i++) {
-        A[i] = malloc(SIZE * sizeof(int));
-        B[i] = malloc(SIZE * sizeof(int));
-        C[i] = malloc(SIZE * sizeof(int));
-        for (int j = 0; j < SIZE; j++) {
-            A[i][j] = 1;
-            B[i][j] = 1;
-            C[i][j] = 0;
-        }
-    }
-
-    signal(SIGINT, sigint_handler);
-
-    for (row = 0; row < SIZE; row++) {
-        for (col = 0; col < SIZE; col++) {
-            for (int k = 0; k < SIZE; k++) {
-                C[row][col] += A[row][k] * B[k][col];
-            }
-            sleep(1);  // Задержка в 1 секунду
-        }
-    }
-
-    printf("Multiplication complete.\n");
-
-    // Освобождение памяти
-    for (int i = 0; i < SIZE; i++) {
-        free(A[i]);
-        free(B[i]);
-        free(C[i]);
-    }
-    free(A);
-    free(B);
-    free(C);
-
-    return 0;
 }
