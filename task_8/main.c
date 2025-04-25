c7e949e9b54a07bb001cc2dd9e14eb27a438ef04
Valerie <13012005lera@gmail.com>
2025-04-25 19:41:20 +0300

    task_8_multiply_matrices

--- task_8/main.c
+++ task_8/main.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 
 #define SIZE 1000
 
-int row = 0, col = 0;
-int sigint_count = 0;
-
-void sigint_handler(int sig) {
-    if (sigint_count == 0) {
-        printf("\n[signal()] Caught SIGINT! Current row = %d, col = %d\n", row, col);
-        signal(SIGINT, SIG_DFL);  // Восстановить поведение по умолчанию
-        sigint_count++;
+volatile sig_atomic_t sigint_received = 0;
+volatile int current_i = 0;
+volatile int current_j = 0;
+//используем signal()
+void handle_sigint(int sig) {
+    if (!sigint_received) {
+        printf("\nSIGINT received. Current indices: i = %d, j = %d\n", current_i, current_j);
+        sigint_received = 1;
+        signal(SIGINT, SIG_DFL); // восстанавливаем поведение по умолчанию
     }
+}
+
+int main() {
+    static int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            A[i][j] = 1;
+            B[i][j] = 2;
+        }
+    }
+    //перемножаем матрицы
+    for (int i = 0; i < SIZE; i++) {
+        current_i = i;
+        for (int j = 0; j < SIZE; j++) {
+            current_j = j;
+            C[i][j] = 0;
+            for (int k = 0; k < SIZE; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+            sleep(1);
+        }
+    }
+    return 0;
 }
