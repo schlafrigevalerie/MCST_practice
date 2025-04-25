82ed2b3ce8af927ce61eccb77f6f2723e744900a
Valerie <13012005lera@gmail.com>
2025-04-25 21:50:14 +0300

    task_10_add_error_handling_rpm

--- task_10/main.c
+++ task_10/main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 typedef struct {
@@ -6,11 +7,11 @@ typedef struct {
     int value;
 } Variable;
 
-int get_var_value(char var, Variable *vars, int count) {
-    for (int i = 0; i < count; i++) {
-        if (vars[i].name == var)
-            return vars[i].value;
+int get_var_value(char ch, Variable *vars, int var_count) {
+    for (int i = 0; i < var_count; i++) {
+        if (vars[i].name == ch) return vars[i].value;
     }
+    fprintf(stderr, "Error: Undefined variable '%c'\n", ch);
     return 0;
 }
 
@@ -44,32 +45,59 @@ int precedence(char op) {
     }
 }
 
-void to_rpn(const char *infix, char *postfix) {
-    char ops[512];
-    int top = -1, j = 0;
-    for (int i = 0; infix[i]; i++) {
-        char ch = infix[i];
-        if (isspace(ch)) continue;
-        if (isalpha(ch)) {
-            postfix[j++] = ch;
-        } else if (ch == '(') {
-            ops[++top] = ch;
-        } else if (ch == ')') {
-            while (top >= 0 && ops[top] != '(') {
-                postfix[j++] = ops[top--];
+int to_rpn(const char *expr, char *output) {
+    char stack[512];
+    int top = -1;
+    int out_i = 0;
+    int open_parens = 0;
+
+    for (int i = 0; expr[i]; ++i) {
+        if (isspace(expr[i])) continue;
+
+        if (isalpha(expr[i])) {
+            output[out_i++] = expr[i];
+        } else if (expr[i] == '(') {
+            stack[++top] = expr[i];
+            open_parens++;
+        } else if (expr[i] == ')') {
+            open_parens--;
+            if (open_parens < 0) {
+                fprintf(stderr, "Error: mismatched parentheses\n");
+                return 0;;
             }
-            top--;
+            while (top >= 0 && stack[top] != '(')
+                output[out_i++] = stack[top--];
+            if (top < 0) {
+                fprintf(stderr, "Error: mismatched parentheses\n");
+                return 0;;
+            }
+            top--; // Убрать '('
+        } else if (strchr("!&|", expr[i])) {
+            char op = expr[i];
+            while (top >= 0 && precedence(stack[top]) >= precedence(op))
+                output[out_i++] = stack[top--];
+            stack[++top] = op;
         } else {
-            while (top >= 0 && precedence(ops[top]) >= precedence(ch)) {
-                postfix[j++] = ops[top--];
-            }
-            ops[++top] = ch;
+            fprintf(stderr, "Error: invalid character '%c'\n", expr[i]);
+            return 0;;
         }
     }
-    while (top >= 0) {
-        postfix[j++] = ops[top--];
+
+    if (open_parens != 0) {
+        fprintf(stderr, "Error: mismatched parentheses\n");
+        return 0;;
     }
-    postfix[j] = '\0';
+
+    while (top >= 0) {
+        if (stack[top] == '(') {
+            fprintf(stderr, "Error: mismatched parentheses\n");
+            return 0;;
+        }
+        output[out_i++] = stack[top--];
+    }
+
+    output[out_i] = '\0';
+    return 1;
 }
 
 int eval_rpn(const char *postfix, Variable *vars, int var_count) {
@@ -96,5 +124,33 @@ int eval_rpn(const char *postfix, Variable *vars, int var_count) {
 
 
 int main() {
+    int var_count;
+    printf("Enter number of variables (max 26): ");
+    scanf("%d", &var_count);
+    getchar(); // clear newline
 
+    if (var_count > 26) var_count = 26;
+
+    Variable vars[26];
+    for (int i = 0; i < var_count; i++) {
+        vars[i].name = 'A' + i;
+        printf("%c = ", vars[i].name);
+        scanf("%d", &vars[i].value);
+        getchar();
+    }
+
+    char expr[512];
+    printf("Enter logical expression:\n");
+    fgets(expr, 512, stdin);
+    expr[strcspn(expr, "\n")] = 0; // remove newline
+
+    normalize(expr);
+
+    char rpn[512];
+    to_rpn(expr, rpn);
+
+    int result = eval_rpn(rpn, vars, var_count);
+    printf("RESULT: %d\n", result);
+
+    return 0;
 }
