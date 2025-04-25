2f59431ec9e92fc21fd311a9d89442ef282cf1bc
Valerie <13012005lera@gmail.com>
2025-04-25 18:31:53 +0300

    task_4_valid_elbrus

--- task_4/main.c
+++ task_4/main.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <getopt.h>
-#include <stdbool.h>
  
-const char *valid[] = {"1c+", "2c+", "2c3", "4c", "8c", "16c"};
+int is_valid_elbrus(const char *value) {
+    const char *valid[] = {"1c+", "2c+", "2c3", "4c", "8c", "16c"};
+    for (int i = 0; i < sizeof(valid)/sizeof(valid[0]); i++) {
+        if (strcmp(value, valid[i]) == 0) return 1;
+    }
+    return 0;
+}
 
